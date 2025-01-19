d935e25039f463df3098f5eaf6ce1ad4e7533848
PhilSciMath <leandro.ff7v2@gmail.com>
2025-01-19 20:17:37 -0300

    First commit

---
+++ source/functions.py
@@ -0,0 +1,164 @@
+"""
+    This module contains all the functions used in the tic_tac_toe.py file.
+"""
+
+import subprocess as sp
+
+
+def option_validator():
+    """ Validates player input """
+
+    options = ['1', '2', '3', 'q']
+    selected = ' '
+    while selected not in options:
+        selected = input("'1', '2', '3', or 'q' to quit: ")
+
+    return selected
+
+
+def grid_update(grid, row, col, char):
+    """ This function updates the grid with the given positions. """
+
+    if grid[row][col] == ' ':
+        grid[row][col] = char
+
+    return grid
+
+
+def check_empty(grid, row, col):
+    """ This function checks if the chosen position is empty. """
+
+    if grid[row][col] == ' ':
+        return True
+    else:
+        return False
+
+
+def check_gameover(grid, char):
+    """ This function checks all the conditions for a gameover. """
+
+    # checking the rows
+    for row in grid:
+        if row.count(char) == 3:
+            return True
+
+    # we can do the same if the grid is transposed
+    transposed = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            transposed[i][j] = grid[j][i]
+    for row in transposed:
+        if row.count(char) == 3:
+            return True
+
+    # now the diagonals
+    d1 = []
+    for i in range(len(grid)):
+        d1.append(grid[i][i])
+    if d1.count(char) == 3:
+        return True
+
+    # the anti-diagonal
+    d2 = []
+    for i in range(len(grid)):
+        for j in reversed(range(len(grid))):
+            if i + j == len(grid) - 1:
+                d2.append(grid[i][j])
+    if d2.count(char) == 3:
+        return True
+
+
+def display(grid_map):
+    """ This function tries to show the game grid in a larger grid. It
+    receives the grid_map from the function grid_mapping() and organizes
+    the elements, printing line by line.
+    """
+
+    # clean up the display first:
+    sp.run('clear')
+
+    # it took me hours to come up with this abomination
+    for i in range(0, 6, 1):
+        print(f"{grid_map[i]} | {grid_map[i + 6]} | {grid_map[i + 12]}")
+        if i == 5:
+            print(24 * '-')
+    for i in range(18, 24, 1):
+        print(f"{grid_map[i]} | {grid_map[i + 6]} | {grid_map[i + 12]}")
+        if i == 23:
+            print(24 * '-')
+    for i in range(36, 42, 1):
+        print(f"{grid_map[i]} | {grid_map[i + 6]} | {grid_map[i + 12]}")
+
+
+def grid_mapping(grid):
+    """ This function maps the game grid to the larger ascii
+        characters bellow. It returns a simple list.
+    """
+
+    # I want the game to look bigger in the terminal
+    x = [
+        "#    #",
+        " #  # ",
+        "  ##  ",
+        "  ##  ",
+        " #  # ",
+        "#    #"
+        ]
+
+    o = [
+        " #### ",
+        "#    #",
+        "#    #",
+        "#    #",
+        "#    #",
+        " #### "
+        ]
+
+    e = [
+        "      ",
+        "      ",
+        "      ",
+        "      ",
+        "      ",
+        "      ",
+        ]
+
+    # this matrix will be filled with the lists x, o and e defined above
+    matrix = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+        ]
+
+    # mapping the matrix to the grid
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == 'x':
+                matrix[i][j] = x
+            elif grid[i][j] == 'o':
+                matrix[i][j] = o
+            else:
+                matrix[i][j] = e
+
+    # unpacks the matrix into a simple list
+    grid_map = []
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            for k in range(6):
+                grid_map.append(matrix[i][j][k])
+
+    return grid_map
+
+
+def show_empty_grid():
+    """
+        In case the player starts first, this shows an empty grid.
+    """
+
+    sp.run('clear')
+    bar = 24 * '-'
+    for i in range(20):
+        if i == 6 or i == 13:
+            print(bar)
+        else:
+            print('       |        |       ')
---
+++ source/tic_tac_toe.py
@@ -0,0 +1,104 @@
+"""
+    I was searching for a small project to code after learning
+    a bit of Python. Then I saw this idea of a tic tac toe game. I didn't see
+    any example beforehand so it is going to be built from scratch.
+"""
+
+
+from random import choice
+import time
+
+import functions as fn
+
+# who starts first?
+player_turn = False
+if input('Do you want to go first? [yes/no]: ').lower()[0] == 'y':
+    player_turn = True
+
+
+# definig the character the user will play
+player_char = ''
+chars = ['x', 'o']
+
+while player_char not in chars:
+    player_char = input("Do you choose 'x' or 'o'? ").lower()
+
+# defining the character for the computer to play
+for c in chars:
+    if c != player_char:
+        com_char = c
+
+grid = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+        ]
+
+# shows an empty grid if player starts first
+if player_turn:
+    fn.show_empty_grid()
+
+# main game loop
+while True:
+
+    # alternating between player and computer
+    if player_turn:
+
+        print("Choose a row.")
+        row = fn.option_validator()
+        if row == 'q':
+            break
+        print("Choose a column.")
+        col = fn.option_validator()
+        if col == 'q':
+            break
+
+        row = int(row) - 1
+        col = int(col) - 1
+
+        # we don't update the grid unless the chosen position is empty
+        if fn.check_empty(grid, row, col):
+            pass
+        else:
+            print("The position is not empty!\n")
+            continue
+
+        # updates the grid, then pass the turn to the computer
+        grid = fn.grid_update(grid, row, col, player_char)
+        player_turn = False
+
+    else:
+        # computer choice
+        values = [0, 1, 2]
+        com_choice = []
+
+        # computer will choose positions untill it finds an empty one
+        while True:
+            com_choice.clear()
+            for i in range(2):
+                com_choice.append(choice(values))
+            row = com_choice[0]
+            col = com_choice[1]
+
+            if fn.check_empty(grid, row, col):
+                break
+
+        # when the position is empty updates the grid, then it's player turn
+        grid = fn.grid_update(grid, row, col, com_char)
+        player_turn = True
+        time.sleep(2)
+
+    # shows the grid
+    result = fn.grid_mapping(grid)
+    fn.display(result)
+
+    # gameover
+    if fn.check_gameover(grid, com_char):
+        print("\nYou lost!")
+        break
+    if fn.check_gameover(grid, player_char):
+        print("\nYou won!")
+        break
+
+
+
